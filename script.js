704fbd191ef84c239db9918a59d1edea8f625497
sandwipmitra <15654738-sandwipmitra@users.noreply.replit.com>
2024-01-02 02:08:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+ const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+  console.log(email);
+  const passwordField = document.getElementById('user-pass');
+  const pass = passwordField.value;
+  console.log(pass);
+  if(email === 'sandy.mitra488@gmail.com' && pass === 'secret$77'){
+    console.log('Valid user')
+  }else{
+    console.log('Invalid user')
+    }
+});
